71c78394e2599048b4fd437d09bcd736a406e2b3
mahdimortuza <mahdi.webx@gmail.com>
2024-08-11 10:14:32 +0600

    adding error handling class

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,20 +1,35 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import express, { Application, NextFunction, Request, Response } from 'express';
+
+import express, { Application, ErrorRequestHandler } from 'express';
+import { ZodError } from 'zod';
 const app: Application = express();
 
-const globalErrorHandler = (
-  err: any,
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  const statusCode = 500,
-    message = err.message || 'Something went wrong!';
+const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  // default values for error
+  let statusCode = 500;
+  let message = err.message || 'Something went wrong!';
+
+  type TErrorSource = {
+    path: string | number;
+    message: string;
+  }[];
+
+  const errorSources: TErrorSource = [
+    {
+      path: '',
+      message: 'Something went wrong!',
+    },
+  ];
+
+  if (err instanceof ZodError) {
+    statusCode = 400;
+    message = 'ami error';
+  }
 
   return res.status(statusCode).json({
     success: false,
     message,
+    errorSources,
     error: err,
   });
 };
