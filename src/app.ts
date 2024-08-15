d10eb4b91637d39096d96c2bf03fe734e672f7d1
mahdimortuza <mahdi.webx@gmail.com>
2024-08-15 20:00:53 +0600

    all done

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,9 @@ const app: Application = express();
 // parsers
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
+app.use(
+  cors({ origin: ['https://heal-io-client.vercel.app'], credentials: true }),
+);
 
 // application routes
 app.use('/api/v1', router);
